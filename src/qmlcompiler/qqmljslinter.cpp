f3d5e44df8a11ce0636b949ec4e7a73cd4cfd5f8
Sami Shalayel <sami.shalayel@qt.io>
2025-07-22 12:13:43 +0200

    qmllint: emit parser warnings via QQmlJSLogger
    We emit all warnings with QQmlJSLogger, so don't use qWarning() for
    parser warnings.
    This makes our parser warnings more beautiful because QQmlJSLogger can
    print the code around the invalid token, for example on unexpected token
    errors.
    
    Also make sure that we use the correct logger to emit the parser
    warning.
    
    Pick-to: 6.8 6.9 6.10
    Fixes: QTBUG-137029
    Change-Id: Ibf2a24cd17c20edac3c20dd44185f18e891eab66
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    Reviewed-by: Olivier De Cannière <olivier.decanniere@qt.io>

--- src/qmlcompiler/qqmljslinter.cpp
+++ src/qmlcompiler/qqmljslinter.cpp
@@ -568,6 +568,11 @@ QQmlJSLinter::LintResult QQmlJSLinter::lintFile(const QString &filename,
     const bool isESModule = lowerSuffix == QLatin1String("mjs");
     const bool isJavaScript = isESModule || lowerSuffix == QLatin1String("js");
 
+    m_logger.reset(new QQmlJSLogger);
+    m_logger->setFilePath(m_useAbsolutePath ? info.absoluteFilePath() : filename);
+    m_logger->setCode(code);
+    m_logger->setSilent(silent || json);
+
     lexer.setCode(code, /*lineno = */ 1, /*qmlMode=*/!isJavaScript);
     QQmlJS::Parser parser(&engine);
 
@@ -576,15 +581,9 @@ QQmlJSLinter::LintResult QQmlJSLinter::lintFile(const QString &filename,
         success = FailedToParse;
         const auto diagnosticMessages = parser.diagnosticMessages();
         for (const QQmlJS::DiagnosticMessage &m : diagnosticMessages) {
-            if (json) {
+            if (json)
                 addJsonWarning(warnings, m, qmlSyntax.name());
-            } else if (!silent) {
-                qWarning().noquote() << QString::fromLatin1("%1:%2:%3: %4")
-                                                .arg(filename)
-                                                .arg(m.loc.startLine)
-                                                .arg(m.loc.startColumn)
-                                                .arg(m.message);
-            }
+            m_logger->log(m.message, qmlSyntax, m.loc);
         }
         return success;
     }
@@ -599,11 +598,6 @@ QQmlJSLinter::LintResult QQmlJSLinter::lintFile(const QString &filename,
     if (!resourceFiles.isEmpty())
         mapper.emplace(resourceFiles);
     m_importer.setResourceFileMapper(mapper.has_value() ? &*mapper : nullptr);
-
-    m_logger.reset(new QQmlJSLogger);
-    m_logger->setFilePath(m_useAbsolutePath ? info.absoluteFilePath() : filename);
-    m_logger->setCode(code);
-    m_logger->setSilent(silent || json);
 
     QQmlJS::LinterVisitor v{ &m_importer, m_logger.get(),
                              QQmlJSImportVisitor::implicitImportDirectory(

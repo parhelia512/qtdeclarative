35f6faf56e7c6af1a6e5ccf784db63d7f6cb7a49
Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>
2025-08-11 15:30:07 +0200

    Use new qt_target_qml_from_svg() function for examples
    This updates weatherforecast and vectorimage examples to
    use the automatic svgtoqml build steps instead of manual
    conversion.
    
    Removes .pro file for weatherforecast. This is a new
    example after the move to CMake and it should have
    never had a .pro file to begin with. We aren't going
    to implement the svgtoqml build step with qmake, so we
    can't keep this working.
    
    Task-number: QTBUG-128915
    Change-Id: I2e2506316d4fc8fb35c227309f50e0c92f2a0e38
    Reviewed-by: Eirik Aavitsland <eirik.aavitsland@qt.io>

--- examples/quick/quickshapes/quickshapes.pro
+++ examples/quick/quickshapes/quickshapes.pro
@@ -1,2 +1,2 @@
 TEMPLATE = subdirs
-SUBDIRS = shapes weatherforecast
+SUBDIRS = shapes
--- examples/quick/vectorimage/generate.bat
+++
@@ -1,1 +0,0 @@
-@svgtoqml -p --copyright-statement "Copyright (C) 2024 The Qt Company Ltd.\nSPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause" -c heart.svg Heart.qml

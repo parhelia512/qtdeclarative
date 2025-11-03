4a2b9d298390adac884729b6066834f202543348
Ulf Hermann <ulf.hermann@qt.io>
2025-11-03 15:14:19 +0100

    qmlformat: Fix string conversion problems
    Assume UTF-8 when dealing with JSON and use QStringView where we can.
    Explicitly create QStrings where we have to.
    
    Amends commit 962614c1c073efbcbc5752d917c1d198e76b2e60.
    
    Task-number: QTCREATORBUG-33305
    Change-Id: I89aeef61b5e6373138fc1b064abb97f0593d6801
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    Reviewed-by: Sami Shalayel <sami.shalayel@qt.io>
    Reviewed-by: Xavier BESSON (Personal) <developer@xavi-b.fr>

--- src/qmlformat/qqmlformatsettings.cpp
+++ src/qmlformat/qqmlformatsettings.cpp
@@ -9,18 +9,21 @@
 #include <QJsonDocument>
 #include <QJsonObject>
 
+using namespace Qt::StringLiterals;
+
 QQmlFormatSettings::QQmlFormatSettings(const QString &toolName) : QQmlToolingSettings(toolName)
 {
     addOption(s_useTabsSetting, false);
     addOption(s_indentWidthSetting, 4);
     addOption(s_maxColumnWidthSetting, -1);
     addOption(s_normalizeSetting, false);
-    addOption(s_newlineSetting, QStringLiteral("native"), QStringList{ "unix", "windows", "macos", "native" });
+    addOption(s_newlineSetting, u"native"_s,
+              QStringList { u"unix"_s, u"windows"_s, u"macos"_s, u"native"_s });
     addOption(s_objectsSpacingSetting, false);
     addOption(s_functionsSpacingSetting, false);
     addOption(s_sortImportsSetting, false);
     addOption(s_singleLineEmptyObjectsSetting, false);
-    addOption(s_semiColonRuleSetting, QStringLiteral("always"), QStringList{ "always", "essential" });
+    addOption(s_semiColonRuleSetting, u"always"_s, QStringList{ u"always"_s, u"essential"_s });
 }
 
 void QQmlFormatSettings::addOption(const QString &name, const QVariant &defaultValue, const QStringList &allowedValues)
@@ -38,18 +41,19 @@ bool QQmlFormatSettings::outputOptions() const
     QJsonArray optionsArray;
     for (auto it = m_values.constBegin(); it != m_values.constEnd(); ++it) {
         QJsonObject option;
-        option[QStringLiteral("name")] = it.key();
-        option[QStringLiteral("value")] = QJsonValue::fromVariant(it.value());
-        option[QStringLiteral("hint")] = it.value().typeName();
+        option[u"name"] = it.key();
+        option[u"value"] = QJsonValue::fromVariant(it.value());
+        option[u"hint"] = QString::fromUtf8(it.value().typeName());
 
         if (it.value().typeId() == QMetaType::QString)
-            option[QStringLiteral("hint")] = m_allowedValues[it.key()].join(',');
+            option[u"hint"] = m_allowedValues[it.key()].join(u',');
 
         optionsArray.append(option);
     }
-    root[QStringLiteral("options")] = optionsArray;
+    root[u"options"] = optionsArray;
 
-    QCommandLineParser::showMessageAndExit(QCommandLineParser::MessageType::Information,
-                                           QJsonDocument(root).toJson(), EXIT_SUCCESS);
+    QCommandLineParser::showMessageAndExit(
+            QCommandLineParser::MessageType::Information,
+            QString::fromUtf8(QJsonDocument(root).toJson()), EXIT_SUCCESS);
     return true;
 }

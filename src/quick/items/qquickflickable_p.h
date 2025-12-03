9c6f72450cf8c9008f4e80b8d68d8d46e761fdc2
Fabian Kosmale <fabian.kosmale@qt.io>
2025-12-03 16:38:53 +0100

    QQuickFlickable: Add missing include for QPointF
    Two recently introduced methods have a defaulted QPointF default
    parameter, so we should ensure that QPointF is complete.
    Amends 20d2ed3d112674cc20202770959ae7e7577f2fb9
    
    Change-Id: Ic376283031202fa967faa9a710f4a89ce3926863
    Reviewed-by: Xavier BESSON (Qt) <xavier.besson@qt.io>
    Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>

--- src/quick/items/qquickflickable_p.h
+++ src/quick/items/qquickflickable_p.h
@@ -1,3 +1,4 @@
+
 // Copyright (C) 2019 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 // Qt-Security score:significant reason:default
@@ -18,6 +19,7 @@
 
 #include "qquickitem.h"
 #include <private/qtquickglobal_p.h>
+#include <QtCore/qpoint.h>
 
 QT_BEGIN_NAMESPACE
 

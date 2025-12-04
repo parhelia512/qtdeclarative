c8707c589518681e9e9737846841aefa5e58e983
Olivier De Cannière <olivier.decanniere@qt.io>
2025-12-04 17:40:49 +0100

    QML: Temporarily allow revisions 13 and 14 of moc in qqmlpropertycache
    The revision change does not break qqmlpropertycache but will cause
    static_asserts failures while the patch adapting to the moc change is
    not merged.
    
    Temporarily allow for both revision and revert to 14 only as soon as
    poosible.
    
    Task-number: QTBUG-142186
    Change-Id: I4ea37bff08f0780148ec7f8958a05f372a6df0f3
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

--- src/qml/qml/qqmlpropertycache.cpp
+++ src/qml/qml/qqmlpropertycache.cpp
@@ -1180,7 +1180,9 @@ int countMetaObjectFields(const QMetaObject &mo, StringVisitor stringVisitor)
 
 } // anonymous namespace
 
-static_assert(QMetaObjectPrivate::OutputRevision == 13, "Check and adjust determineMetaObjectSizes");
+// Temporary leniency to allow for smooth transition of moc change - QTBUG-142186
+static_assert(QMetaObjectPrivate::OutputRevision == 13 || QMetaObjectPrivate::OutputRevision == 14,
+              "Check and adjust determineMetaObjectSizes");
 
 bool QQmlPropertyCache::determineMetaObjectSizes(const QMetaObject &mo, int *fieldCount,
                                                  int *stringCount)

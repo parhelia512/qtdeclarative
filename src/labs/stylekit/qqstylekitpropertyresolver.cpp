parhelia512/qtdeclarative

commit d29963d7f24dca49ca1c2f55fb70cfe461137f5c

StyleKit: avoid calling style->setPalette twice
As it stood, we called style->setPalette twice, first
from readStyleProperty(), then immediately again from
readProperty(). While this has no real performance impact,
it can still be confusing when trying to understand the
code.

This patch will therefore remove the superfluous call, and
at the same time, move the related cacheReaderState() call
to the same place where we sync the palette.

Pick-to: 6.11
Change-Id: Icbd00a76930edc30daaf83ec9b73b61736f62445
Reviewed-by: Doris Verria <doris.verria@qt.io>

src/labs/stylekit/qqstylekitpropertyresolver.cpp
@@ -490,6 +490,11 @@ QVariant QQStyleKitPropertyResolver::readProperty(
      * result in calls to writeStyleProperty(). */
     style->syncPaletteFromReader(styleReader);
 
+    /* Cache the state of the style reader to avoid rebuilding the same helper
+     * structures on subsequent reads. In practice, a single style reader
+     * typically processes many properties in sequence rather than just one. */
+    cacheReaderState(styleReader->controlState());
+
     const QQStyleKitExtendableControlType exactType = styleReader->type();
     const QList<QQStyleKitExtendableControlType> baseTypes = baseTypesForType(exactType);
 
@@ -615,10 +620,7 @@ QVariant QQStyleKitPropertyResolver::readStyleProperty(
                 return value;
         }
 
-        style->syncPaletteFromReader(styleReader);
-        cacheReaderState(styleReader->controlState());
-        const QVariant value = readProperty(ids, styleReader, style);
-        return value;
+        return readProperty(ids, styleReader, style);
     }
 
     Q_UNREACHABLE();

commit a95b95d870823bd5bb9a1a1c401d98213cd42d6b

StyleKit: ensure we cache reader state _before_ rebuilding variations
d29963d7f2 introduced a regression where we would change the order
of code to rebuild variations before caching the current reader state.
This caused a bug to happen since resolving variations includes reading
style properties, and without caching the state first, we would
in some cases resolve the wrong varations for the current reader.

This patch will restore the order of the code as it wore in the
initial patch.

Pick-to: 6.11
Change-Id: I7f56490a592828bdd2b6929d4683ae2b4c0fbdd1
Reviewed-by: Doris Verria <doris.verria@qt.io>

src/labs/stylekit/qqstylekitpropertyresolver.cpp
@@ -482,9 +482,6 @@ QVariant QQStyleKitPropertyResolver::readPropertyInStyle(
 QVariant QQStyleKitPropertyResolver::readProperty(
     const PropertyPathIds &ids, QQStyleKitReader *styleReader, QQStyleKitStyle *style)
 {
-    if (styleReader->m_effectiveVariationsDirty)
-        rebuildVariationsForReader(styleReader, style);
-
     /* Sync the palette of the style with the palette of the current reader. Note
      * that this can cause palette bindings in the style to change, which will
      * result in calls to writeStyleProperty(). */
@@ -494,6 +491,9 @@ QVariant QQStyleKitPropertyResolver::readProperty(
      * structures on subsequent reads. In practice, a single style reader
      * typically processes many properties in sequence rather than just one. */
     cacheReaderState(styleReader->controlState());
+
+    if (styleReader->m_effectiveVariationsDirty)
+        rebuildVariationsForReader(styleReader, style);
 
     const QQStyleKitExtendableControlType exactType = styleReader->type();
     const QList<QQStyleKitExtendableControlType> baseTypes = baseTypesForType(exactType);
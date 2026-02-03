parhelia512/qtdeclarative

commit ab83b2d2d34095bd56ca0e9e044374d314277321

FileDialog: Comply with accessibility insights for Windows
Accessibility Insights for Windows is a Windows application that will
check the UI accessibility tree for any potential issues or
anti-patterns.

The tool would complain about the FileDialog having a button without a
name, in the FolderBreadcrumbBar.

The SideBar buttonDelegate would use a Button with an IconImage
contentItem, which the tool would complain about, since the Button
control then wouldn't have a name.

The FileDialogDelegate would also expose every label that it uses to
QAccessible. I'd assume users of screen readers would find it
overwhelming that every delegate has more than 4 different labels that
are all exposed to external assistive technologies. The most important
item to expose is the delegate itself, the sub-labels showing the file
size and last modified date can probably be ignored.

Pick-to: 6.10 6.8
Change-Id: Iad3fbf4a4178934214028214947506eb9d8c2651
Reviewed-by: Mitch Curtis <mitch.curtis@qt.io>

src/quickdialogs/quickdialogsquickimpl/qml/+Fusion/FileDialogDelegate.qml
@@ -41,6 +41,8 @@ DialogsQuickImpl.FileDialogDelegate {
 
     required property int fileDetailRowWidth
 
+    Accessible.name: fileName
+
     contentItem: DialogsQuickImpl.FileDialogDelegateLabel {
         delegate: control
         fileDetailRowTextColor: control.highlighted ? Fusion.highlightedText(control.palette) : control.palette.text

src/quickdialogs/quickdialogsquickimpl/qml/+Fusion/FolderBreadcrumbBar.qml
@@ -67,6 +67,7 @@ DialogsQuickImpl.FolderBreadcrumbBar {
         width: height
         height: Math.max(implicitHeight, control.contentItem.height)
         focusPolicy: Qt.TabFocus
+        Accessible.name: qsTr("Up")
     }
     textField: TextField {
         text: (control.dialog as DialogsQuickImpl.FileDialogImpl)?.selectedFile

src/quickdialogs/quickdialogsquickimpl/qml/+Fusion/SideBar.qml
@@ -62,6 +62,8 @@ DialogsQuickImpl.SideBar {
 
         required property int index
         required property string folderName
+
+        Accessible.name: folderName
     }
 
     separatorDelegate: Item {

src/quickdialogs/quickdialogsquickimpl/qml/+Imagine/FileDialogDelegate.qml
@@ -46,6 +46,8 @@ DialogsQuickImpl.FileDialogDelegate {
 
     required property int fileDetailRowWidth
 
+    Accessible.name: fileName
+
     contentItem: DialogsQuickImpl.FileDialogDelegateLabel {
         delegate: control
         fileDetailRowTextColor: control.icon.color

src/quickdialogs/quickdialogsquickimpl/qml/+Imagine/FolderBreadcrumbBar.qml
@@ -51,6 +51,7 @@ DialogsQuickImpl.FolderBreadcrumbBar {
         icon.width: 16
         icon.height: 16
         focusPolicy: Qt.TabFocus
+        Accessible.name: qsTr("Up")
     }
     textField: TextField {
         text: (control.dialog as DialogsQuickImpl.FileDialogImpl)?.selectedFile

src/quickdialogs/quickdialogsquickimpl/qml/+Imagine/SideBar.qml
@@ -42,6 +42,8 @@ DialogsQuickImpl.SideBar {
 
         required property int index
         required property string folderName
+
+        Accessible.name: folderName
     }
 
     separatorDelegate: Item {

src/quickdialogs/quickdialogsquickimpl/qml/+Material/FileDialogDelegate.qml
@@ -38,6 +38,8 @@ DialogsQuickImpl.FileDialogDelegate {
 
     required property int fileDetailRowWidth
 
+    Accessible.name: fileName
+
     contentItem: DialogsQuickImpl.FileDialogDelegateLabel {
         delegate: control
         fileDetailRowTextColor: control.Material.hintTextColor

src/quickdialogs/quickdialogsquickimpl/qml/+Material/FolderBreadcrumbBar.qml
@@ -65,6 +65,7 @@ DialogsQuickImpl.FolderBreadcrumbBar {
         icon.height: 16
         width: height
         focusPolicy: Qt.TabFocus
+        Accessible.name: qsTr("Up")
     }
     textField: TextField {
         text: (control.dialog as DialogsQuickImpl.FileDialogImpl)?.selectedFile

commit 1248bbdaea0cfd3943abd5f0fa23e5f601867e2a

Fix FileDialog with -no-accessibility build flag
ab83b2d2d34095bd56ca0e9e044374d314277321 caused the FileDialog
to stop working for -no-accessibility builds. That's because the
Accessible attached object is not recognised as an existing type
in those builds, and thus the component can't be created.

To fix it, we'll instead have to set the accessible name in C++.

As a drive-by, make all non-FINAL properties of
QQuickFileDialogImplAttached FINAL.

Fixes: QTBUG-142511
Pick-to: 6.11 6.10 6.8
Change-Id: Iea679a4acfe56e9938149dddf3410c4a55f8b28b
Reviewed-by: MohammadHossein Qanbari <mohammad.qanbari@qt.io>

src/quickdialogs/quickdialogsquickimpl/qml/+Fusion/FileDialog.qml
@@ -65,6 +65,7 @@ FileDialogImpl {
           in C++) because we'd need to copy (and hence duplicate code in) DialogButtonBox.qml.
     */
     FileDialogImpl.buttonBox: buttonBox
+    FileDialogImpl.filterLabel: filterLabel
     FileDialogImpl.nameFiltersComboBox: nameFiltersComboBox
     FileDialogImpl.fileDialogListView: fileDialogListView
     FileDialogImpl.breadcrumbBar: breadcrumbBar
@@ -196,12 +197,12 @@ FileDialogImpl {
         }
 
         Label {
+            id: filterLabel
             text: qsTr("Filter")
             Layout.column: 0
             Layout.row: 1
             Layout.leftMargin: 12
             Layout.bottomMargin: 12
-            Accessible.labelFor: nameFiltersComboBox
         }
 
 

src/quickdialogs/quickdialogsquickimpl/qml/+Fusion/FileDialogDelegate.qml
@@ -41,8 +41,6 @@ DialogsQuickImpl.FileDialogDelegate {
 
     required property int fileDetailRowWidth
 
-    Accessible.name: fileName
-
     contentItem: DialogsQuickImpl.FileDialogDelegateLabel {
         delegate: control
         fileDetailRowTextColor: control.highlighted ? Fusion.highlightedText(control.palette) : control.palette.text

src/quickdialogs/quickdialogsquickimpl/qml/+Fusion/FolderBreadcrumbBar.qml
@@ -67,7 +67,6 @@ DialogsQuickImpl.FolderBreadcrumbBar {
         width: height
         height: Math.max(implicitHeight, control.contentItem.height)
         focusPolicy: Qt.TabFocus
-        Accessible.name: qsTr("Up")
     }
     textField: TextField {
         text: (control.dialog as DialogsQuickImpl.FileDialogImpl)?.selectedFile

src/quickdialogs/quickdialogsquickimpl/qml/+Fusion/SideBar.qml
@@ -62,8 +62,6 @@ DialogsQuickImpl.SideBar {
 
         required property int index
         required property string folderName
-
-        Accessible.name: folderName
     }
 
     separatorDelegate: Item {

src/quickdialogs/quickdialogsquickimpl/qml/+Imagine/FileDialog.qml
@@ -62,6 +62,7 @@ FileDialogImpl {
     }
 
     FileDialogImpl.buttonBox: buttonBox
+    FileDialogImpl.filterLabel: filterLabel
     FileDialogImpl.nameFiltersComboBox: nameFiltersComboBox
     FileDialogImpl.fileDialogListView: fileDialogListView
     FileDialogImpl.breadcrumbBar: breadcrumbBar
@@ -182,13 +183,13 @@ FileDialogImpl {
         }
 
         Label {
+            id: filterLabel
             text: qsTr("Filter")
 
             Layout.column: 0
             Layout.row: 1
             Layout.leftMargin: 16
             Layout.bottomMargin: 16
-            Accessible.labelFor: nameFiltersComboBox
         }
 
         ComboBox {

src/quickdialogs/quickdialogsquickimpl/qml/+Imagine/FileDialogDelegate.qml
@@ -47,8 +47,6 @@ DialogsQuickImpl.FileDialogDelegate {
 
     required property int fileDetailRowWidth
 
-    Accessible.name: fileName
-
     contentItem: DialogsQuickImpl.FileDialogDelegateLabel {
         delegate: control
         fileDetailRowTextColor: control.icon.color

src/quickdialogs/quickdialogsquickimpl/qml/+Imagine/FolderBreadcrumbBar.qml
@@ -51,7 +51,6 @@ DialogsQuickImpl.FolderBreadcrumbBar {
         icon.width: 16
         icon.height: 16
         focusPolicy: Qt.TabFocus
-        Accessible.name: qsTr("Up")
     }
     textField: TextField {
         text: (control.dialog as DialogsQuickImpl.FileDialogImpl)?.selectedFile

src/quickdialogs/quickdialogsquickimpl/qml/+Imagine/SideBar.qml
@@ -42,8 +42,6 @@ DialogsQuickImpl.SideBar {
 
         required property int index
         required property string folderName
-
-        Accessible.name: folderName
     }
 
     separatorDelegate: Item {

src/quickdialogs/quickdialogsquickimpl/qml/+Material/FileDialog.qml
@@ -56,6 +56,7 @@ FileDialogImpl {
     }
 
     FileDialogImpl.buttonBox: buttonBox
+    FileDialogImpl.filterLabel: filterLabel
     FileDialogImpl.nameFiltersComboBox: nameFiltersComboBox
     FileDialogImpl.fileDialogListView: fileDialogListView
     FileDialogImpl.breadcrumbBar: breadcrumbBar
@@ -166,12 +167,12 @@ FileDialogImpl {
         }
 
         Label {
+            id: filterLabel
             text: qsTr("Filter")
 
             Layout.row: 1
             Layout.topMargin: fileNameTextField.visible ? 0 : 12
             Layout.leftMargin: 20
-            Accessible.labelFor: nameFiltersComboBox
         }
 
         ComboBox {

src/quickdialogs/quickdialogsquickimpl/qml/+Material/FileDialogDelegate.qml
@@ -38,8 +38,6 @@ DialogsQuickImpl.FileDialogDelegate {
 
     required property int fileDetailRowWidth
 
-    Accessible.name: fileName
-
     contentItem: DialogsQuickImpl.FileDialogDelegateLabel {
         delegate: control
         fileDetailRowTextColor: control.Material.hintTextColor

src/quickdialogs/quickdialogsquickimpl/qml/+Material/FolderBreadcrumbBar.qml
@@ -65,7 +65,6 @@ DialogsQuickImpl.FolderBreadcrumbBar {
         icon.height: 16
         width: height
         focusPolicy: Qt.TabFocus
-        Accessible.name: qsTr("Up")
     }
     textField: TextField {
         text: (control.dialog as DialogsQuickImpl.FileDialogImpl)?.selectedFile

src/quickdialogs/quickdialogsquickimpl/qml/+Material/SideBar.qml
@@ -47,8 +47,6 @@ DialogsQuickImpl.SideBar {
 
         required property int index
         required property string folderName
-
-        Accessible.name: folderName
     }
 
     separatorDelegate: Item {

src/quickdialogs/quickdialogsquickimpl/qml/+Universal/FileDialog.qml
@@ -56,6 +56,7 @@ FileDialogImpl {
     }
 
     FileDialogImpl.buttonBox: buttonBox
+    FileDialogImpl.filterLabel: filterLabel
     FileDialogImpl.nameFiltersComboBox: nameFiltersComboBox
     FileDialogImpl.fileDialogListView: fileDialogListView
     FileDialogImpl.breadcrumbBar: breadcrumbBar
@@ -169,13 +170,13 @@ FileDialogImpl {
         }
 
         Label {
+            id: filterLabel
             text: qsTr("Filter")
 
             Layout.row: 1
             Layout.column: 0
             Layout.leftMargin: 24
             Layout.bottomMargin: 24
-            Accessible.labelFor: nameFiltersComboBox
         }
 
         ComboBox {

src/quickdialogs/quickdialogsquickimpl/qml/+Universal/FileDialogDelegate.qml
@@ -39,8 +39,6 @@ DialogsQuickImpl.FileDialogDelegate {
 
     required property int fileDetailRowWidth
 
-    Accessible.name: fileName
-
     contentItem: DialogsQuickImpl.FileDialogDelegateLabel {
         delegate: control
         fileDetailRowTextColor: control.icon.color

src/quickdialogs/quickdialogsquickimpl/qml/+Universal/FolderBreadcrumbBar.qml
@@ -63,7 +63,6 @@ DialogsQuickImpl.FolderBreadcrumbBar {
         icon.height: 16
         width: height
         focusPolicy: Qt.TabFocus
-        Accessible.name: qsTr("Up")
     }
     textField: TextField {
         text: (control.dialog as DialogsQuickImpl.FileDialogImpl)?.selectedFile

src/quickdialogs/quickdialogsquickimpl/qml/+Universal/SideBar.qml
@@ -48,8 +48,6 @@ DialogsQuickImpl.SideBar {
 
         required property int index
         required property string folderName
-
-        Accessible.name: folderName
     }
 
     separatorDelegate: Item {

src/quickdialogs/quickdialogsquickimpl/qml/FileDialog.qml
@@ -72,6 +72,7 @@ FileDialogImpl {
           in C++) because we'd need to copy (and hence duplicate code in) DialogButtonBox.qml.
     */
     FileDialogImpl.buttonBox: buttonBox
+    FileDialogImpl.filterLabel: filterLabel
     FileDialogImpl.nameFiltersComboBox: nameFiltersComboBox
     FileDialogImpl.fileDialogListView: fileDialogListView
     FileDialogImpl.breadcrumbBar: breadcrumbBar
@@ -185,12 +186,12 @@ FileDialogImpl {
             }
 
             Label {
+                id: filterLabel
                 text: qsTr("Filter")
 
                 Layout.row: 1
                 Layout.column: 0
                 Layout.leftMargin: 20
-                Accessible.labelFor: nameFiltersComboBox
             }
 
             ComboBox {

src/quickdialogs/quickdialogsquickimpl/qml/FileDialogDelegate.qml
@@ -41,8 +41,6 @@ DialogsQuickImpl.FileDialogDelegate {
 
     property int fileDetailRowWidth
 
-    Accessible.name: fileName
-
     contentItem: DialogsQuickImpl.FileDialogDelegateLabel {
         delegate: control
         fileDetailRowTextColor: control.icon.color

src/quickdialogs/quickdialogsquickimpl/qml/FileDialogDelegateLabel.qml
@@ -37,12 +37,10 @@ Item {
                 color: root.delegate.icon.color
                 y: (parent.height - height) / 2
             }
-            Label {
+            Text {
                 text: root.delegate.fileName
                 color: root.delegate.icon.color
                 y: (parent.height - height) / 2
-
-                Accessible.ignored: true
             }
         }
 
@@ -52,7 +50,7 @@ Item {
             width: fileDetailRowWidth - x - root.delegate.leftPadding
             implicitHeight: childrenRect.height
 
-            Label {
+            Text {
                 text: {
                     const fileSize = root.delegate.fileSize;
                     return fileSize > Number.MAX_SAFE_INTEGER
@@ -61,16 +59,12 @@ Item {
                 }
                 font.pixelSize: root.delegate.font.pixelSize * 0.75
                 color: root.fileDetailRowTextColor
-
-                Accessible.ignored: true
             }
-            Label {
+            Text {
                 text: Qt.formatDateTime(root.delegate.fileModified)
                 font.pixelSize: root.delegate.font.pixelSize * 0.75
                 color: root.fileDetailRowTextColor
                 x: parent.width - width
-
-                Accessible.ignored: true
             }
         }
     }

src/quickdialogs/quickdialogsquickimpl/qml/FolderBreadcrumbBar.qml
@@ -62,7 +62,6 @@ DialogsQuickImpl.FolderBreadcrumbBar {
         icon.height: 16
         width: height
         focusPolicy: Qt.TabFocus
-        Accessible.name: qsTr("Up")
     }
     textField: TextField {
         text: (control.dialog as DialogsQuickImpl.FileDialogImpl)?.selectedFile

src/quickdialogs/quickdialogsquickimpl/qml/SideBar.qml
@@ -55,12 +55,9 @@ DialogsQuickImpl.SideBar {
             defaultIconColor: buttonDelegateRoot.icon.color
             color: defaultIconColor
         }
-
         background: DelegateBackground {
             control: buttonDelegateRoot
         }
-
-        Accessible.name: folderName
     }
 
     separatorDelegate: Item {

src/quickdialogs/quickdialogsquickimpl/qquickfiledialogdelegate.cpp
@@ -10,6 +10,9 @@
 #include <QtGui/qpa/qplatformtheme.h>
 #include <QtQml/QQmlFile>
 #include <QtQml/qqmlexpression.h>
+#if QT_CONFIG(accessibility)
+#include <QtQuick/private/qquickaccessibleattached_p.h>
+#endif
 #include <QtQuick/private/qquicklistview_p.h>
 #include <QtQuick/private/qquickitemview_p_p.h>
 #include "qquicksidebar_p.h"
@@ -137,6 +140,10 @@ void QQuickFileDialogDelegate::setFile(const QUrl &file)
 
     d->file = adjustedFile;
     emit fileChanged();
+#if QT_CONFIG(accessibility)
+    if (QQuickAccessibleAttached *accessibleAttached = QQuickControlPrivate::accessibleAttached(this))
+        accessibleAttached->setName(file.fileName());
+#endif
 }
 
 void QQuickFileDialogDelegate::keyReleaseEvent(QKeyEvent *event)

src/quickdialogs/quickdialogsquickimpl/qquickfiledialogimpl.cpp
@@ -10,6 +10,9 @@
 #include <QtGui/qpa/qplatformtheme.h>
 #include <QtQml/qqmlinfo.h>
 #include <QtQml/qqmlfile.h>
+#if QT_CONFIG(accessibility)
+#include <QtQuick/private/qquickaccessibleattached_p.h>
+#endif
 #include <QtQuick/private/qquickitemview_p_p.h>
 #include <QtQuickTemplates2/private/qquickdialogbuttonbox_p_p.h>
 #include <QtQuickTemplates2/private/qquickpopupitem_p_p.h>
@@ -18,7 +21,6 @@
 
 #include "qquickfiledialogdelegate_p.h"
 #include "qquickfolderbreadcrumbbar_p.h"
-#include "qquicksidebar_p.h"
 
 QT_BEGIN_NAMESPACE
 
@@ -550,6 +552,14 @@ void QQuickFileDialogImpl::componentComplete()
     }
 
     keyNavigationAttached->setTab(attached->breadcrumbBar()->upButton());
+
+#if QT_CONFIG(accessibility)
+    auto *label = attached->filterLabel();
+    auto *comboBox = attached->nameFiltersComboBox();
+    if (label && comboBox)
+        if (QQuickAccessibleAttached *accessibleAttached = QQuickControlPrivate::accessibleAttached(label))
+            accessibleAttached->setLabelFor(comboBox);
+#endif
 }
 
 void QQuickFileDialogImpl::itemChange(QQuickItem::ItemChange change, const QQuickItem::ItemChangeData &data)
@@ -712,6 +722,24 @@ void QQuickFileDialogImplAttached::setNameFiltersComboBox(QQuickComboBox *nameFi
         d, &QQuickFileDialogImplAttachedPrivate::nameFiltersComboBoxItemActivated);
 
     emit nameFiltersComboBoxChanged();
+}
+
+QQuickLabel *QQuickFileDialogImplAttached::filterLabel() const
+{
+    Q_D(const QQuickFileDialogImplAttached);
+    return d->filterLabel;
+}
+
+void QQuickFileDialogImplAttached::setFilterLabel(QQuickLabel *label)
+{
+    Q_D(QQuickFileDialogImplAttached);
+
+    if (d->filterLabel == label)
+        return;
+
+    d->filterLabel = label;
+
+    emit filterLabelChanged();
 }
 
 QString QQuickFileDialogImplAttached::selectedNameFilter() const

src/quickdialogs/quickdialogsquickimpl/qquickfiledialogimpl_p.h
@@ -108,9 +108,10 @@ class Q_QUICKDIALOGS2QUICKIMPL_EXPORT QQuickFileDialogImplAttached : public QObj
 {
     Q_OBJECT
     Q_PROPERTY(QQuickDialogButtonBox *buttonBox READ buttonBox WRITE setButtonBox NOTIFY buttonBoxChanged FINAL)
-    Q_PROPERTY(QQuickComboBox *nameFiltersComboBox READ nameFiltersComboBox WRITE setNameFiltersComboBox NOTIFY nameFiltersComboBoxChanged)
-    Q_PROPERTY(QQuickListView *fileDialogListView READ fileDialogListView WRITE setFileDialogListView NOTIFY fileDialogListViewChanged)
-    Q_PROPERTY(QQuickFolderBreadcrumbBar *breadcrumbBar READ breadcrumbBar WRITE setBreadcrumbBar NOTIFY breadcrumbBarChanged)
+    Q_PROPERTY(QQuickComboBox *nameFiltersComboBox READ nameFiltersComboBox WRITE setNameFiltersComboBox NOTIFY nameFiltersComboBoxChanged FINAL)
+    Q_PROPERTY(QQuickLabel *filterLabel READ filterLabel WRITE setFilterLabel NOTIFY filterLabelChanged FINAL)
+    Q_PROPERTY(QQuickListView *fileDialogListView READ fileDialogListView WRITE setFileDialogListView NOTIFY fileDialogListViewChanged FINAL)
+    Q_PROPERTY(QQuickFolderBreadcrumbBar *breadcrumbBar READ breadcrumbBar WRITE setBreadcrumbBar NOTIFY breadcrumbBarChanged FINAL)
     Q_PROPERTY(QQuickLabel *fileNameLabel READ fileNameLabel WRITE setFileNameLabel NOTIFY fileNameLabelChanged FINAL)
     Q_PROPERTY(QQuickTextField *fileNameTextField READ fileNameTextField WRITE setFileNameTextField NOTIFY fileNameTextFieldChanged FINAL)
     Q_PROPERTY(QQuickDialog *overwriteConfirmationDialog READ overwriteConfirmationDialog WRITE setOverwriteConfirmationDialog NOTIFY overwriteConfirmationDialogChanged FINAL)
@@ -128,6 +129,9 @@ public:
 
     QQuickComboBox *nameFiltersComboBox() const;
     void setNameFiltersComboBox(QQuickComboBox *nameFiltersComboBox);
+
+    QQuickLabel *filterLabel() const;
+    void setFilterLabel(QQuickLabel *label);
 
     QString selectedNameFilter() const;
     void selectNameFilter(const QString &filter);
@@ -153,6 +157,7 @@ public:
 Q_SIGNALS:
     void buttonBoxChanged();
     void nameFiltersComboBoxChanged();
+    void filterLabelChanged();
     void fileDialogListViewChanged();
     void breadcrumbBarChanged();
     void fileNameLabelChanged();

src/quickdialogs/quickdialogsquickimpl/qquickfiledialogimpl_p_p.h
@@ -84,6 +84,7 @@ public:
 
     QPointer<QQuickDialogButtonBox> buttonBox;
     QPointer<QQuickComboBox> nameFiltersComboBox;
+    QPointer<QQuickLabel> filterLabel;
     QPointer<QQuickListView> fileDialogListView;
     QPointer<QQuickFolderBreadcrumbBar> breadcrumbBar;
     QPointer<QQuickLabel> fileNameLabel;

src/quickdialogs/quickdialogsquickimpl/qquickfolderbreadcrumbbar.cpp
@@ -12,8 +12,12 @@
 #endif
 #include <QtGui/private/qguiapplication_p.h>
 #include <QtQml/QQmlFile>
+#if QT_CONFIG(accessibility)
+#include <QtQuick/private/qquickaccessibleattached_p.h>
+#endif
 #include <QtQuick/private/qquicktextinput_p.h>
 #include <QtQuickTemplates2/private/qquickabstractbutton_p.h>
+#include <QtQuickTemplates2/private/qquickcontrol_p_p.h>
 #include <QtQuickTemplates2/private/qquickpopupitem_p_p.h>
 #include <QtQuickTemplates2/private/qquickshortcutcontext_p_p.h>
 
@@ -606,6 +610,11 @@ void QQuickFolderBreadcrumbBar::setUpButton(QQuickAbstractButton *upButton)
 
         QObjectPrivate::connect(d->upButton.data(), &QQuickAbstractButton::clicked,
             d, &QQuickFolderBreadcrumbBarPrivate::goUp);
+#if QT_CONFIG(accessibility)
+        QQuickAccessibleAttached *accessibleAttached = qobject_cast<QQuickAccessibleAttached*>(qmlAttachedPropertiesObject<QQuickAccessibleAttached>(upButton, true));
+        if (Q_LIKELY(accessibleAttached))
+            accessibleAttached->setName(QCoreApplication::translate("FileDialog", "Up"));
+#endif
     }
     if (!d->upButton.isExecuting())
         emit upButtonChanged();

src/quickdialogs/quickdialogsquickimpl/qquicksidebar.cpp
@@ -4,12 +4,13 @@
 
 #include "qquicksidebar_p.h"
 #include "qquicksidebar_p_p.h"
-#include "qquickfiledialogimpl_p_p.h"
 #include <QtQml/qqmllist.h>
 #if QT_CONFIG(settings)
 #include <QtCore/qsettings.h>
 #endif
-
+#if QT_CONFIG(accessibility)
+#include <QtQuick/private/qquickaccessibleattached_p.h>
+#endif
 #include <QtQuickTemplates2/private/qquickaction_p.h>
 #include <QtQuickTemplates2/private/qquickcontextmenu_p.h>
 
@@ -258,6 +259,10 @@ void QQuickSideBarPrivate::repopulate()
                 QObjectPrivate::connect(button, &QQuickAbstractButton::clicked, this,
                                         &QQuickSideBarPrivate::buttonClicked);
                 updateIconSourceAndSize(button, iconUrl);
+#if QT_CONFIG(accessibility)
+                if (QQuickAccessibleAttached *accessibleAttached = QQuickControlPrivate::accessibleAttached(button))
+                    accessibleAttached->setName(displayName);
+#endif
             }
             insertItem(q->count(), buttonItem);
         }
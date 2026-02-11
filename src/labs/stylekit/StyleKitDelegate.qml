parhelia512/qtdeclarative

commit 6a400f8b301720a7eb775942e16666c3057c558d

StyleKit: implement StyleKitDelegate in c++ instead of QML
StyleKitDelegate.qml contains JS code that inspects what
it needs to draw, and instantiates the items needed in order
to do so. From profiling, implementing this logic in c++
instead, will reduce the start-up time for an application
with 60 checkboxes from ~1150 ms to ~940 ms.

This patch will therefore reimplement StyleKitDelegate in
c++.

Pick-to: 6.11
Change-Id: I64485d23e47f51447e7789853658f9b6828322b3
Reviewed-by: Doris Verria <doris.verria@qt.io>

src/labs/stylekit/StyleKitDelegate.qml
@@ -1,151 +1,13 @@
-// Copyright (C) 2025 The Qt Company Ltd.
+// Copyright (C) 2026 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
-// Qt-Security score:significant reason:default
 
-import QtQuick
-import QtQuick.Effects
-import QtQuick.Controls.impl
 import Qt.labs.StyleKit
-import Qt.labs.StyleKit.impl
 
-/* This delegate takes care of loading the individual elements that are needed
-    in order to draw it according to its configuration in the style.
-    Since some delegates have a gradient, others an image, and some perhaps
-    both, we use dynamic loading to reduce memory usage. We could use Loaders
-    for this, but from testing, using JS appears visually more performant.
-    TODO: move most of this code to c++? Collapse all elements into a single
-    scene-graph node? Redraw everything from a single update, rather than using
-    property bindings?
-*/
-Item {
-    id: root
-
-    // If an implicit size has been set on the delegate in the style, it
-    // wins. Otherwise we use the implicit size of the image
-    implicitWidth: __imageInstance ? __imageInstance.implicitWidth : 0
-    implicitHeight: __imageInstance ? __imageInstance.implicitHeight : 0
-    width: parent.width
-    height: parent.height
-    opacity: delegateProperties.opacity
-
-    required property QtObject control
-    required property StyleKitDelegateProperties delegateProperties
-
-    property Item __colorInstance: null
-    property Item __gradientInstance: null
-    property Item __imageInstance: null
-
-    onDelegatePropertiesChanged: {
-        instantiateColorDelegate()
-        instantiateGradientDelegate()
-        instantiateImageDelegate()
-    }
-
-    Connections {
-        target: delegateProperties
-        function onColorChanged() { instantiateColorDelegate() }
-        function onGradientChanged() { instantiateGradientDelegate() }
-    }
-
-    Connections {
-        target: delegateProperties.image
-        function onSourceChanged() { instantiateImageDelegate() }
-        function onColorChanged() { instantiateImageDelegate() }
-    }
-
-    Component {
-        id: colorDelegate
-        Rectangle {
-            required property StyleKitDelegateProperties delegateProperties
-            width: parent.width
-            height: parent.height
-            topLeftRadius: delegateProperties.topLeftRadius
-            topRightRadius: delegateProperties.topRightRadius
-            bottomLeftRadius: delegateProperties.bottomLeftRadius
-            bottomRightRadius: delegateProperties.bottomRightRadius
-            color: delegateProperties.color
-            border.width: delegateProperties.border.width
-            border.color: delegateProperties.border.color
-        }
-    }
-
-    Component {
-        id: gradientDelegate
-        Rectangle {
-            required property StyleKitDelegateProperties delegateProperties
-            width: parent.width
-            height: parent.height
-            topLeftRadius: delegateProperties.topLeftRadius
-            topRightRadius: delegateProperties.topRightRadius
-            bottomLeftRadius: delegateProperties.bottomLeftRadius
-            bottomRightRadius: delegateProperties.bottomRightRadius
-            border.width: delegateProperties.border.width
-            border.color: delegateProperties.border.color
-            gradient: delegateProperties.gradient
-            visible: delegateProperties.gradient !== null
-        }
-    }
-
-    Component {
-        id: imageDelegate
-        ColorImage {
-            required property StyleKitDelegateProperties delegateProperties
-            width: parent.width
-            height: parent.height
-            color: delegateProperties.image.color
-            source: delegateProperties.image.source
-            fillMode: delegateProperties.image.fillMode
-            visible: status === Image.Ready
-        }
-    }
-
-    function instantiateColorDelegate() {
-        if (__colorInstance)
-            return
-
-        // Delay instantiating element until needed
-        if (delegateProperties.color.a === 0 &&
-                (delegateProperties.border.color.a === 0
-                 || delegateProperties.border.width === 0))
-            return
-
-        /* Note: we adjust z to ensure the elements are stacked correct, no matter the
-         * order in which they are instantiated. And we use negative z values to ensure
-         * that any children (with a default z === 0) of a "subclass" of the StyleKitDelegate
-         * ends up on top of this again. */
-        let prevInstance = __colorInstance
-        __colorInstance = colorDelegate.createObject(root, { z: -3, delegateProperties: root.delegateProperties })
-        if (prevInstance)
-            prevInstance.destroy()
-    }
-
-
-    function instantiateGradientDelegate() {
-        if (__gradientInstance)
-            return
-
-        // Delay instantiating element until needed
-        if (delegateProperties.gradient === null)
-            return
-
-        let prevInstance = __gradientInstance
-        __gradientInstance = gradientDelegate.createObject(root, { z: -2, delegateProperties: root.delegateProperties })
-        if (prevInstance)
-            prevInstance.destroy()
-    }
-
-    function instantiateImageDelegate() {
-        if (__imageInstance)
-            return
-
-        // Delay instantiating element until needed
-        if (delegateProperties.image.source === "" || delegateProperties.image.color.a === 0)
-            return
-
-        let prevInstance = __imageInstance
-        __imageInstance = imageDelegate.createObject(root, { z: -1, delegateProperties: root.delegateProperties })
-        if (prevInstance)
-            prevInstance.destroy()
-    }
-
+StyleKitDelegateBase {
+    // These properties are bound here in QML, rather than in StyleKitDelegateBase
+    // (C++), so that a custom delegate that embeds a StyleKitDelegate as a child
+    // (to create overlay or underlay effects) can override them to handle how and
+    // where to apply the transform.
+    scale: delegateProperties.scale
+    rotation: delegateProperties.rotation
 }

src/labs/stylekit/qqstylekitdelegate.cpp
@@ -1,0 +1,225 @@
+// Copyright (C) 2026 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#include "qqstylekitdelegate_p.h"
+#include "qqstylekitcontrolproperties_p.h"
+
+#include <QtQuick/private/qquickrectangle_p.h>
+#include <QtQuick/private/qquickimplicitsizeitem_p_p.h>
+#include <QtQuickControls2Impl/private/qquickcolorimage_p.h>
+
+QT_BEGIN_NAMESPACE
+
+QQStyleKitDelegate::QQStyleKitDelegate(QQuickItem *parent)
+    : QQuickImplicitSizeItem(*new QQuickImplicitSizeItemPrivate, parent)
+{
+}
+
+QQStyleKitDelegateProperties *QQStyleKitDelegate::delegateProperties() const
+{
+    return m_delegateProperties;
+}
+
+void QQStyleKitDelegate::setDelegateProperties(QQStyleKitDelegateProperties *delegateProperties)
+{
+    if (m_delegateProperties == delegateProperties)
+        return;
+
+    if (m_delegateProperties)
+        disconnect(m_delegateProperties, nullptr, this, nullptr);
+
+    m_delegateProperties = delegateProperties;
+
+    if (!qmlEngine(this)) {
+        qmlWarning(this) << "Unable to draw delegate: no QQmlEngine found";
+        return;
+    }
+
+    maybeCreateColor();
+    maybeCreateGradient();
+    maybeCreateImage();
+    updateImplicitSize();
+
+    connect(m_delegateProperties, &QQStyleKitDelegateProperties::implicitWidthChanged, this, &QQStyleKitDelegate::updateImplicitSize);
+    connect(m_delegateProperties, &QQStyleKitDelegateProperties::implicitHeightChanged, this, &QQStyleKitDelegate::updateImplicitSize);
+
+    emit delegatePropertiesChanged();
+}
+
+void QQStyleKitDelegate::updateImplicitSize()
+{
+    if (!m_delegateProperties)
+        return;
+
+    /* The implicit size is determined by the following priority:
+     * 1. Explicit implicit size set on StyleKitDelegateProperties
+     * 2. Implicit size of the image (if present)
+     * 3. Zero
+     * The implicit size is read-only because it's calculated in C++ from internal
+     * child items that are intentionally not exposed to QML. */
+    const qreal impWidthInStyle = m_delegateProperties->implicitWidth();
+    const qreal impHeightInStyle = m_delegateProperties->implicitHeight();
+    setImplicitWidth(impWidthInStyle > 0 || !m_imageOverlay ? impWidthInStyle : m_imageOverlay->implicitWidth());
+    setImplicitHeight(impHeightInStyle > 0 || !m_imageOverlay ? impHeightInStyle : m_imageOverlay->implicitHeight());
+}
+
+void QQStyleKitDelegate::maybeCreateColor()
+{
+    if (m_colorOverlay)
+        return;
+    if (!m_delegateProperties)
+        return;
+    if (m_delegateProperties->color().alpha() == 0) {
+        connect(m_delegateProperties, &QQStyleKitDelegateProperties::colorChanged,
+                this, &QQStyleKitDelegate::maybeCreateColor, Qt::UniqueConnection);
+        return;
+    }
+
+    disconnect(m_delegateProperties, &QQStyleKitDelegateProperties::colorChanged,
+            this, &QQStyleKitDelegate::maybeCreateColor);
+
+    QQmlEngine *engine = qmlEngine(this);
+    Q_ASSERT(engine);
+    static QQmlComponent *component = nullptr;
+    if (!component || component->engine() != engine) {
+        delete component;
+        component = new QQmlComponent(engine);
+        const QString qmlCode = QString::fromUtf8(R"(
+            import QtQuick
+            Rectangle {
+                z: -3
+                width: parent.width
+                height: parent.height
+                visible: delegateProperties.visible
+                color: delegateProperties.color
+                topLeftRadius: delegateProperties.topLeftRadius
+                topRightRadius: delegateProperties.topRightRadius
+                bottomLeftRadius: delegateProperties.bottomLeftRadius
+                bottomRightRadius: delegateProperties.bottomRightRadius
+                border.width: delegateProperties.border.width
+                border.color: delegateProperties.border.color
+            }
+        )");
+        component->setData(qmlCode.toUtf8(), QUrl());
+        Q_ASSERT_X(!component->isError(), __FUNCTION__, component->errorString().toUtf8().constData());
+    }
+
+    QQmlContext *ctx = QQmlEngine::contextForObject(this);
+    m_colorOverlay = qobject_cast<QQuickItem*>(component->beginCreate(ctx));
+    Q_ASSERT(m_colorOverlay);
+    m_colorOverlay->setParent(this);
+    m_colorOverlay->setParentItem(this);
+    component->completeCreate();
+}
+
+void QQStyleKitDelegate::maybeCreateGradient()
+{
+    /* Unlike a Rectangle, a StyleKitDelegate draws both the color and the gradient at
+     * the same time. This allows a style to define them independently. That way you can
+     * define a common semi-transparent grayscale gradient once for a delegate in the style
+     * (e.g for control.background.gradient), and then tint it with different colors for
+     * different controls, states, or themes (e.g for button.hovered.background.color). */
+    if (m_gradientOverlay)
+        return;
+    if (!m_delegateProperties)
+        return;
+    if (!m_delegateProperties->gradient()) {
+        connect(m_delegateProperties, &QQStyleKitDelegateProperties::gradientChanged,
+                this, &QQStyleKitDelegate::maybeCreateGradient, Qt::UniqueConnection);
+        return;
+    }
+
+    disconnect(m_delegateProperties, &QQStyleKitDelegateProperties::gradientChanged,
+               this, &QQStyleKitDelegate::maybeCreateGradient);
+
+    QQmlEngine *engine = qmlEngine(this);
+    Q_ASSERT(engine);
+    static QQmlComponent *component = nullptr;
+    if (!component || component->engine() != engine) {
+        delete component;
+        component = new QQmlComponent(engine);
+        const QString qmlCode = QString::fromUtf8(R"(
+            import QtQuick
+            Rectangle {
+                z: -2
+                width: parent.width
+                height: parent.height
+                visible: delegateProperties.visible
+                color: "transparent"
+                gradient: delegateProperties.gradient
+                topLeftRadius: delegateProperties.topLeftRadius
+                topRightRadius: delegateProperties.topRightRadius
+                bottomLeftRadius: delegateProperties.bottomLeftRadius
+                bottomRightRadius: delegateProperties.bottomRightRadius
+                border.width: delegateProperties.border.width
+                border.color: delegateProperties.border.color
+            }
+        )");
+        component->setData(qmlCode.toUtf8(), QUrl());
+        Q_ASSERT_X(!component->isError(), __FUNCTION__, component->errorString().toUtf8().constData());
+    }
+
+    QQmlContext *ctx = QQmlEngine::contextForObject(this);
+    m_gradientOverlay = qobject_cast<QQuickItem*>(component->beginCreate(ctx));
+    Q_ASSERT(m_gradientOverlay);
+    m_gradientOverlay->setParent(this);
+    m_gradientOverlay->setParentItem(this);
+    component->completeCreate();
+}
+
+void QQStyleKitDelegate::maybeCreateImage()
+{
+    if (m_imageOverlay)
+        return;
+    if (!m_delegateProperties)
+        return;
+    if (m_delegateProperties->image()->source().isEmpty()
+        || m_delegateProperties->image()->color().alpha() == 0) {
+        connect(m_delegateProperties->image(), &QQStyleKitImageProperties::sourceChanged,
+                this, &QQStyleKitDelegate::maybeCreateImage, Qt::UniqueConnection);
+        connect(m_delegateProperties->image(), &QQStyleKitImageProperties::colorChanged,
+                this, &QQStyleKitDelegate::maybeCreateImage, Qt::UniqueConnection);
+        return;
+    }
+
+    disconnect(m_delegateProperties->image(), &QQStyleKitImageProperties::sourceChanged,
+               this, &QQStyleKitDelegate::maybeCreateImage);
+    disconnect(m_delegateProperties->image(), &QQStyleKitImageProperties::colorChanged,
+               this, &QQStyleKitDelegate::maybeCreateImage);
+
+    QQmlEngine *engine = qmlEngine(this);
+    Q_ASSERT(engine);
+    static QQmlComponent *component = nullptr;
+    if (!component || component->engine() != engine) {
+        delete component;
+        component = new QQmlComponent(engine);
+        const QString qmlCode = QString::fromUtf8(R"(
+            import QtQuick.Controls.impl
+            ColorImage {
+                z: -1
+                width: parent.width
+                height: parent.height
+                visible: delegateProperties.visible
+                color: delegateProperties.image.color
+                source: delegateProperties.image.source
+                fillMode: delegateProperties.image.fillMode
+            }
+        )");
+        component->setData(qmlCode.toUtf8(), QUrl());
+        Q_ASSERT_X(!component->isError(), __FUNCTION__, component->errorString().toUtf8().constData());
+    }
+
+    QQmlContext *ctx = QQmlEngine::contextForObject(this);
+    m_imageOverlay = qobject_cast<QQuickItem*>(component->beginCreate(ctx));
+    m_imageOverlay->setParent(this);
+    m_imageOverlay->setParentItem(this);
+    component->completeCreate();
+
+    updateImplicitSize();
+    connect(m_imageOverlay, &QQuickItem::implicitWidthChanged, this, &QQStyleKitDelegate::updateImplicitSize);
+    connect(m_imageOverlay, &QQuickItem::implicitHeightChanged, this, &QQStyleKitDelegate::updateImplicitSize);
+}
+
+QT_END_NAMESPACE
+
+#include "moc_qqstylekitdelegate_p.cpp"

examples/quickcontrols/stylekit/styles/CustomDelegates.qml
@@ -28,17 +28,20 @@ Style {
     }
 
     component UnderlayDelegate : Item {
-        // If the base type is not a StyleKitDelegate, the delegate must implement two required
-        // properties: 'control' and 'delegateProperties'. The former points to the Qt Quick Control
-        // that uses the delegate, and the latter points to the StyleKitDelegateProperties that
-        // describes how it should be styled.
-        required property QtObject control
+        /* Custom delegates that don't inherit from StyleKitDelegate can optionally
+         * declare 'delegateProperties' and 'control' properties. Use delegateProperties
+         * to bind to style attributes like color, radius, and opacity. Use control
+         * to access the Quick Control the owns the delegate. */
         required property StyleKitDelegateProperties delegateProperties
+        required property QtObject control
 
-        width: parent.width
-        height: parent.height
         implicitWidth: delegateProperties.implicitWidth
         implicitHeight: delegateProperties.implicitHeight
+        width: parent.width
+        height: parent.height
+        scale: delegateProperties.scale
+        rotation: delegateProperties.rotation
+        visible: delegateProperties.visible
 
         Rectangle {
             id: underlay
@@ -64,7 +67,7 @@ Style {
         }
 
         StyleKitDelegate {
-            control: parent.control
+            // Embed a StyleKitDelegate to render the standard delegate on top of the custom one
             delegateProperties: parent.delegateProperties
         }
     }
@@ -73,21 +76,24 @@ Style {
         // You can pass your own properties from the style as well, like
         // here, where we use 'isFirst' to tell whether the delegate instance
         // represents the first or the second handle, in case of a RangeSlider.
+        // By adding a 'control' property, we can access the slider's value(s).
+        id: sliderHandle
         property bool isFirstHandle: false
+        required property QtObject control
 
         Text {
-            rotation: control.vertical ? -90 : 0
+            rotation: sliderHandle.control.vertical ? -90 : 0
             anchors.centerIn: parent
             font.pixelSize: 9
             text: {
-                if (control instanceof T.RangeSlider) {
-                    if (isFirstHandle)
-                        return control.first.value.toFixed(0)
+                if (sliderHandle.control instanceof T.RangeSlider) {
+                    if (sliderHandle.isFirstHandle)
+                        return sliderHandle.control.first.value.toFixed(0)
                     else
-                        return control.second.value.toFixed(0)
+                        return sliderHandle.control.second.value.toFixed(0)
                 }
 
-                return control.value.toFixed(0)
+                return sliderHandle.control.value.toFixed(0)
             }
         }
     }
@@ -99,7 +105,6 @@ Style {
         width: parent.width
         height: parent.height
 
-        required property QtObject control
         required property StyleKitDelegateProperties delegateProperties
 
         // The following properties are used by the shader (noise.frag)
@@ -126,9 +131,12 @@ Style {
 
         StyleKitDelegate {
             id: unifiedSourceItem
-            control: parent.control
             delegateProperties: parent.delegateProperties
+            width: parent.width
+            height: parent.height
             visible: false
+            rotation: 0.0
+            scale: 1.0
         }
     }
 
@@ -136,7 +144,6 @@ Style {
         implicitWidth: delegateProperties.implicitWidth
         implicitHeight: delegateProperties.implicitHeight
 
-        required property QtObject control
         required property StyleKitDelegateProperties delegateProperties
 
         // The following properties are used by the shader (wave.frag)
@@ -165,7 +172,6 @@ Style {
     }
 
     component CustomShadowDelegate : Item {
-        required property QtObject control
         required property StyleKitDelegateProperties delegateProperties
 
         x: delegateProperties.shadow.verticalOffset
@@ -222,6 +228,7 @@ Style {
     }
 
     textField {
+        background.shadow.color: "lightgray"
         background.shadow.verticalOffset: 14
         background.shadow.horizontalOffset: 14
         background.shadow.delegate: CustomShadowDelegate {}

examples/quickcontrols/stylekit/styles/Vitrum.qml
@@ -13,8 +13,10 @@ Style {
         implicitHeight: unifiedSourceItem.implicitHeight
         width: parent.width
         height: parent.height
+        scale: delegateProperties.scale
+        rotation: delegateProperties.rotation
+        visible: delegateProperties.visible
 
-        required property QtObject control;
         required property StyleKitDelegateProperties delegateProperties
 
         readonly property bool isDarkBg: {
@@ -41,9 +43,12 @@ Style {
 
         StyleKitDelegate {
             id: unifiedSourceItem
-            control: noiseDelegate.control
             delegateProperties: noiseDelegate.delegateProperties
+            width: parent.width
+            height: parent.height
             visible: false
+            rotation: 0.0
+            scale: 1.0
         }
     }
 

src/labs/stylekit/CMakeLists.txt
@@ -18,6 +18,7 @@ qt_internal_add_qml_module(QtQuickStyleKit
         qqstylekitcontrolproperties_p.h qqstylekitcontrolproperties.cpp
         qqstylekitcustomcontrol_p.h qqstylekitcustomcontrol.cpp
         qqstylekitdebug_p.h qqstylekitdebug.cpp
+        qqstylekitdelegate_p.h qqstylekitdelegate.cpp
         qqstylekitglobal_p.h qqstylekitglobal.cpp
         qqstylekitpalette_p.h qqstylekitpalette.cpp
         qqstylekitfont_p.h qqstylekitfont.cpp
@@ -68,4 +69,5 @@ qt_internal_add_qml_module(QtQuickStyleKit
         Qt6::QuickPrivate
         Qt6::QuickTemplates2
         Qt6::QuickTemplates2Private
+        Qt6::QuickControls2ImplPrivate
 )

src/labs/stylekit/qqstylekitdelegate_p.h
@@ -1,0 +1,56 @@
+// Copyright (C) 2026 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
+
+#ifndef QQSTYLEKITDELEGATE_P_H
+#define QQSTYLEKITDELEGATE_P_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include <QtQuick/private/qquickimplicitsizeitem_p.h>
+#include <QtQml/qqmlparserstatus.h>
+
+QT_BEGIN_NAMESPACE
+
+class QQStyleKitDelegateProperties;
+
+class QQStyleKitDelegate : public QQuickImplicitSizeItem
+{
+    Q_OBJECT
+    Q_PROPERTY(QQStyleKitDelegateProperties *delegateProperties READ delegateProperties WRITE setDelegateProperties NOTIFY delegatePropertiesChanged REQUIRED FINAL)
+    QML_NAMED_ELEMENT(StyleKitDelegateBase)
+
+public:
+    explicit QQStyleKitDelegate(QQuickItem *parent = nullptr);
+
+    QQStyleKitDelegateProperties *delegateProperties() const;
+    void setDelegateProperties(QQStyleKitDelegateProperties *delegateProperties);
+
+signals:
+    void delegatePropertiesChanged();
+
+private:
+    void updateImplicitSize();
+    void maybeCreateColor();
+    void maybeCreateGradient();
+    void maybeCreateImage();
+
+private:
+    QPointer<QQStyleKitDelegateProperties> m_delegateProperties;
+    QPointer<QQuickItem> m_colorOverlay;
+    QPointer<QQuickItem> m_gradientOverlay;
+    QPointer<QQuickItem> m_imageOverlay;
+};
+
+QT_END_NAMESPACE
+
+#endif // QQSTYLEKITDELEGATE_P_H

commit 2ef75775a7abf51eaa3d29494b8de8065a4a8e0e

StyleKit: set visible in StyleKitDelegate, rather than on the child items
A regression from 6a400f8b30 caused the checkboxes in the Vitrum
style to not draw as checked if they were first unchecked, then
checked again.

The reason is that Vitrum is applying a shader effect on the
indicator and therefore needs to hard-code visible on the
StyleKitDelegate to false, since it's used as a shader source.
But since the children toggled it's own visibility independent of
this, it caused the shader to fail drawing correctly.

This patch will therefore skip binding visibility on the child
items, and do it on the parent StyleKitDelegate instead. This allows
the style to hard-code the visibilty to false when StyleKitDelegate
is used as a shader source, which fixes the regression. And it also
saves each delegate instance from a few extra bindings.

Pick-to: 6.11
Change-Id: I2d98c6ddaa72e9298973d1dcd65be3eb14198674
Reviewed-by: Doris Verria <doris.verria@qt.io>

src/labs/stylekit/StyleKitDelegate.qml
@@ -10,4 +10,5 @@ StyleKitDelegateBase {
     // where to apply the transform.
     scale: delegateProperties.scale
     rotation: delegateProperties.rotation
+    visible: delegateProperties.visible
 }

src/labs/stylekit/qqstylekitdelegate.cpp
@@ -90,7 +90,6 @@ void QQStyleKitDelegate::maybeCreateColor()
                 z: -3
                 width: parent.width
                 height: parent.height
-                visible: delegateProperties.visible
                 color: delegateProperties.color
                 topLeftRadius: delegateProperties.topLeftRadius
                 topRightRadius: delegateProperties.topRightRadius
@@ -144,7 +143,6 @@ void QQStyleKitDelegate::maybeCreateGradient()
                 z: -2
                 width: parent.width
                 height: parent.height
-                visible: delegateProperties.visible
                 color: "transparent"
                 gradient: delegateProperties.gradient
                 topLeftRadius: delegateProperties.topLeftRadius
@@ -199,7 +197,6 @@ void QQStyleKitDelegate::maybeCreateImage()
                 z: -1
                 width: parent.width
                 height: parent.height
-                visible: delegateProperties.visible
                 color: delegateProperties.image.color
                 source: delegateProperties.image.source
                 fillMode: delegateProperties.image.fillMode
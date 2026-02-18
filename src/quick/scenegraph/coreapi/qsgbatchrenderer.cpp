04c5704ba8eccf8a48a5682f517285a82e92dae6
Axel Spoerl <axel.spoerl@qt.io>
2026-02-18 21:42:54 +0100

    Quick/Scenegraph/CoreAPI: Add default security headers (QUIP-23)
    QUIP-23: Mark files with default security headers, unless marked
    already.
    
    Change-Id: I94a4027ccde1b852b96e6dc5201771a525efb086
    Reviewed-by: Richard Moe Gustavsen <richard.gustavsen@qt.io>

--- src/quick/scenegraph/coreapi/qsgbatchrenderer.cpp
+++ src/quick/scenegraph/coreapi/qsgbatchrenderer.cpp
@@ -2,6 +2,7 @@
 // Copyright (C) 2016 Jolla Ltd, author: <gunnar.sletta@jollamobile.com>
 // Copyright (C) 2016 Robin Burchell <robin.burchell@viroteck.net>
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #include "qsgbatchrenderer_p.h"
 

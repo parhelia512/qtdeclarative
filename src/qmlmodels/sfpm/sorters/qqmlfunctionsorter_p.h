e637f20e8fbabeea2c093c6ebc39721649544b3d
Axel Spoerl <axel.spoerl@qt.io>
2026-02-19 15:15:55 +0100

    QmlModels: Add default security headers (QUIP-23)
    QUIP-23: Mark files with default security headers,
    unless marked already.
    
    Change-Id: I2094fe53e78049c0dd182377f74f92de819c3a65
    Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>

--- src/qmlmodels/sfpm/sorters/qqmlfunctionsorter_p.h
+++ src/qmlmodels/sfpm/sorters/qqmlfunctionsorter_p.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2025 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef QQMLFUNCTIONSORTER_P_H
 #define QQMLFUNCTIONSORTER_P_H
